7ec9e64bc35e246ee93f5201d5a6c340f7ef0131
matheuscla <matheuscla@yahoo.com.br>
2017-01-27 21:22:43 -0200

    Add admin feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   scope "/admin_user" do
-    resources :users
+    resources :users do
+      post :add_to_family, on: :collection
+    end
   end
-
 
   root to: "families#index"
 end
