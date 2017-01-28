01d6f9c7ab637e7f137af2c8ad1119f4f2f77cd5
matheuscla <matheuscla@yahoo.com.br>
2017-01-27 22:25:03 -0200

    Add family member

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -3,9 +3,17 @@ class FamiliesController < ApplicationController
 
   def index
     if current_user.family_id == 0
-      redirect_to users_path
+
     else
       @family = Family.find(current_user.family_id)
     end
+  end
+
+  def add_to_family
+    @family = current_user.family_id
+    @user = User.find(params[:id])
+    @user.family_id = @family
+    @user.save
+    redirect_to users_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   scope "/admin_user" do
-    resources :users do
-      post :add_to_family, on: :collection
-    end
+    resources :users
   end
+
+  resources :families do
+    post :add_to_family, on: :collection
+  end
+
 
   root to: "families#index"
 end
