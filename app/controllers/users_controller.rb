ed20cfeac118258fbaec195e7fd0497750376f64
matheuscla <matheuscla@yahoo.com.br>
2017-01-28 11:17:15 -0200

    Add search user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
   def index
-   @users = User.all
+    @users = User.all
+    if params[:search]
+      @users = User.search(params[:search]).order("created_at DESC")
+    else
+      @users = User.all.order("created_at DESC")
+    end
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def search
+    @users = User.find_by(params[:value])
+    render 'results'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.search(search)
+    where("first_name LIKE ? OR last_name LIKE ? OR email LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+  end
 end
