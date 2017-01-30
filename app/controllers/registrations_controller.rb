1747949046437e19185806c42b6ab8a184f69e54
pedatela <lucaspedatela@gmail.com>
2017-01-30 16:01:27 -0200

    Change Devise Params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
-  private
+
+  
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :mi, :address1, :address2, :city, :zip, :country, :email, :password, :password_confirmation)
   end
